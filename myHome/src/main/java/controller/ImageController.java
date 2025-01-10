b421625f660426c2d477f4c57c83be0380a318f3
tlshy002 <tlshy002@gmail.com>
2025-01-10 14:17:25 +0900

    36커밋: 디버깅완료 정상작동 + jsp와 컨트롤러 매핑(bbsimage)

--- myHome/src/main/java/controller/ImageController.java
+++ myHome/src/main/java/controller/ImageController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,8 +13,15 @@ import model.Imagebbs;
 @Controller
 public class ImageController {
 	
+	@RequestMapping(value="/image/write.html")
+	public ModelAndView write(@Valid @ModelAttribute("bbsimage") Imagebbs imagebbs,
+								BindingResult br) {
+		ModelAndView mav = new ModelAndView("index");
+		return mav;
+	}
+	
 	@RequestMapping(value="/image/imageWrite.html")
-	public ModelAndView write() {
+	public ModelAndView writeform() {
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("bbsimage", new Imagebbs());
 		mav.addObject("BODY","imageWriteForm.jsp"); //include페이지에 들어감
